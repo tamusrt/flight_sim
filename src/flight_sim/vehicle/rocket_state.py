f48abc32e535400767c4ea9ccca78ddc4ca4d4d6
bzady80 <aliabadibrady3@gmail.com>
2026-09-01 23:54:41 -0500

    Update rocket_state.py

--- src/flight_sim/vehicle/rocket_state.py
+++ src/flight_sim/vehicle/rocket_state.py
@@ -1,27 +1,25 @@
+"""Environment module containing rocket_state data structures"""
 from dataclasses import dataclass
 
 @dataclass
 class RocketState:
+    """Dataclass representing conditions regarding current state of vehicle throughout flight"""
     #Position Coordinates (meters)
     x_pos: float = 0.0
     y_pos: float = 0.0
     z_pos: float = 0.0
-    
     #Velocity (m/s)
     x_vel: float = 0.0
     y_vel: float = 0.0
     z_vel: float = 0.0
-    
     #Orientation Quaternions
     q_w: float = 1.0  #Magnitude must be exactly 1, or else math won't work
     q_x: float = 0.0
     q_y: float = 0.0
     q_z: float = 0.0
-    
     #Angular Velocities (rad/sec)
     roll_rate: float = 0.0
     pitch_rate: float = 0.0
     yaw_rate: float = 0.0
-    
     #Mass Properties (kg)
     current_mass: float = 0.0
