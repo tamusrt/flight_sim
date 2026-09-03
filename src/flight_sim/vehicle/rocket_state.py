eeb0fcd159911a5101a7b6f1111932e1b62b2a8e
Brady Aliabadi <aliabadibrady3@gmail.com>
2026-09-02 23:06:21 -0500

    Changes made to rocket_state.py... already discussed on call

--- src/flight_sim/vehicle/rocket_state.py
+++ src/flight_sim/vehicle/rocket_state.py
@@ -1,34 +1,39 @@
 """Vehicle module containing the 6-DOF state structures of the rocket."""
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
+import numpy as np
+
+
+def zero_array():
+    return np.zeros(3)
 
 
 @dataclass
-class RocketState:
-    """Dataclass representing the current state of the vehicle."""
+class Quaternion:
+    """Quaternion representaion to track orientation"""
 
-    # pylint: disable=too-many-instance-attributes
-
-    # Position Coordinates (meters)
-    x_pos: float = 0.0
-    y_pos: float = 0.0
-    z_pos: float = 0.0
-
-    # Velocity (m/s)
-    x_vel: float = 0.0
-    y_vel: float = 0.0
-    z_vel: float = 0.0
-
-    # Orientation Quaternions
     q_w: float = 1.0
     q_x: float = 0.0
     q_y: float = 0.0
     q_z: float = 0.0
 
+
+@dataclass
+class RocketState:
+    """Dataclass representing the current 6-DOF state of the vehicle."""
+
+    # Position Coordinates (meters)
+    position: np.ndarray = field(default_factory=zero_array)
+
+    # Velocity (m/s)
+    velocity: np.ndarray = field(default_factory=zero_array)
+
     # Angular Velocities (rad/sec)
-    roll_rate: float = 0.0
-    pitch_rate: float = 0.0
-    yaw_rate: float = 0.0
+    angular_velocity: np.ndarray = field(default_factory=zero_array)
+
+    # Orientatiom
+    orientation: Quaternion = field(default_factory=Quaternion)
 
     # Mass Properties (kg)
     current_mass: float = 0.0
