2e5d4f6613a8f0ceb6a61834fd8f0ed0f3d4bea7
Brady Aliabadi <aliabadibrady3@gmail.com>
2026-09-21 00:09:10 -0500

    Docstring issues fixed in engine.py

--- src/flight_sim/vehicle/engine.py
+++ src/flight_sim/vehicle/engine.py
@@ -10,25 +10,29 @@ class Engine(ABC):
 
     @abstractmethod
     def get_thrust(self, time: float) -> float:
+        """Calculate the thrust produced by the engine at a given time."""
         pass
 
     @abstractmethod
     def get_mass_flow(self, time: float, current_thrust: float) -> float:
+        """Calculate the rate of mass depletion for the engine."""
         pass
 
 
 @dataclass
 class SolidEngine(Engine):
-    """Information for solid motors"""
+    """Implementation for solid motors"""
 
     thrust_curve: Callable[[float], float]
     total_propellant_mass: float
     total_impulse: float
 
     def get_thrust(self, time: float) -> float:
+        """Calculate the thrust produced by the solid engine at a given time."""
         return float(self.thrust_curve(time))
 
     def get_mass_flow(self, time: float, current_thrust: float) -> float:
+        """Calculate the rate of mass depletion for the solid engine."""
         if self.total_impulse > 0:
             return -current_thrust * (self.total_propellant_mass / self.total_impulse)
         return 0.0
